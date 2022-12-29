4e33be6a4a200cc49c95b24b3d4175992524983d
vglfr <vf.velt@gmail.com>
2022-12-29 22:21:07 +0200

    Fix commited files

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
-import Numc (parse)
+import Data.ByteString.Char8 (putStrLn)
+import Prelude hiding (putStrLn)
+
+import Numc (add, fstr, main', printf, toIR, toMod)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  parse
+  s <- toIR . toMod $ [fstr, printf, add, main']
+  putStrLn s
